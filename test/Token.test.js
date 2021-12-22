ed97c67e22acaf115123b5c6e32988d10c22d1cf
araf179 <arafshihab@outlook.com>
2021-12-22 14:25:38 -0500

    Adding smart contracts

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,3 +1,7 @@
+const { entries, result } = require("lodash");
+const { isValidElement } = require("react");
+var web3 = require("web3");
+
 // eslint-disable-next-line no-undef
 const Token = artifacts.require('./Token');
 
@@ -5,12 +9,18 @@ require('chai')
 .use(require("chai-as-promised"))
 .should()
 
+const tokens = (n) => {
+    return new web3.utils.BN(
+        web3.utils.toWei(n.toString(), 'ether')
+    )
+}
+
 // eslint-disable-next-line no-undef
-contract('Token', ([deployer, reciever]) => {
+contract('Token', ([deployer, reciever, exchange]) => {
     const name = 'DApp Token';
     const symbol = 'DAPP';
     const decimals = '18';
-    const totalSupply = '1000000000000000000000000';
+    const totalSupply = tokens(1000000);
     let token;
 
     beforeEach(async () => {
@@ -36,28 +46,66 @@ contract('Token', ([deployer, reciever]) => {
 
         it("tracks the total supply", async () => {
             const result = await token.totalSupply()
-            result.toString().should.equal(totalSupply)
+            result.toString().should.equal(totalSupply.toString())
         })
 
         it("asigns the total sujpply to the depployer", async () => {
             const result = await token.balanceOf(deployer)
-            result.toString().should.equal(totalSupply)
+            result.toString().should.equal(totalSupply.toString())
         })
     })
 
     describe('sending tokens', () => {
+        let amount;
+        let result;
+        
+        beforeEach(async () => {
+            amount = tokens(100)
+            result = await token.transfer(reciever, amount, {from: deployer})
+        })
+
         it('transfers token balance', async () => {
             let balanceOf = await token.balanceOf(deployer);
             console.log('deployer alance', balanceOf.toString())
             balanceOf = await token.balanceOf(reciever)
             console.log('reciever balance', balanceOf.toString())
-
-            await token.transfer(reciever, '100000000000000000000000', {from: deployer})
-
+            await token.transfer(reciever, tokens(100), {from: deployer})
             balanceOf = await token.balanceOf(deployer);
-            console.log('deployer balance after', balanceOf.toString())
+            balanceOf.toString().should.equal(tokens(999800).toString())
             balanceOf = await token.balanceOf(reciever)
-            console.log('reciever balance after', balanceOf.toString())
+            balanceOf.toString().should.equal(tokens(200).toString())
+        })
+
+        it('emits a transfer event', async () => {
+            const log = result.logs[0]
+            log.event.should.eq("Transfer")
+            const event = log.args
+            event.from.toString().should.equal(deployer, 'from is correct')
+            event.to.should.equal(reciever, 'to is correct')
+            event.value.toString().should.equal(amount.toString(), 'value is correct')
+        })
+
+        it('rejects insufficient balances', async () => {
+            let invalidAmount;
+            invalidAmount = tokens(1000000000)
+            await token.transfer(reciever, invalidAmount, { from: deployer }).should.be.rejectedWith('VM Exception while processing transaction: revert')
+        })
+    })
+    
+    describe('Approving tokens', () => {
+        let result;
+        let amount;
+
+        beforeEach(async () => {
+            amount = tokens{100};
+            result = await token.approve(exchange, amount, {from: deployer});
+        })
+
+        describe('success', () => {
+            it('allocates an allowance for delegated token spending on exchange', async () => {
+                const allowance = await token.allowance(deployer, exchange)
+                allowance.toString().should.equal(amount.toString());
+            })
         })
     })
 })
